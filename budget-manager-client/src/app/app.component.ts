56e8dbc01b75edf063f05eadaa6e833dc7f3f479
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2024-12-20 20:21:47 +0100

    Button wrapper component created

--- budget-manager-client/src/app/app.component.ts
+++ budget-manager-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from "./shared/components/button/button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ CommonModule, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
