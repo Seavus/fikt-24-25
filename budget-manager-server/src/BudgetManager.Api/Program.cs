094240a824a17fd661fcb4ba6e35de63eca5870b
Zoran Gjorgjievski <zoran.gjorgjievski@qinshift.com>
2024-12-22 10:23:46 +0100

    Add basic DI classes and test controller

--- budget-manager-server/src/BudgetManager.Api/Program.cs
+++ budget-manager-server/src/BudgetManager.Api/Program.cs
@@ -4,15 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication()
     .AddInfrastructure(builder.Configuration);
 
-builder.Services.AddControllers();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseInfrastructure();
 
 await app.RunAsync();
--- budget-manager-server/src/BudgetManager.Infrastructure/DependencyInjection.cs
+++ budget-manager-server/src/BudgetManager.Infrastructure/DependencyInjection.cs
@@ -4,6 +4,25 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        return services
+                .AddApiServies();
+    }
+
+    public static IServiceCollection AddApiServies(this IServiceCollection services)
+    {
+        services.AddControllers();
+
         return services;
+    }
+
+    public static IApplicationBuilder UseInfrastructure(this WebApplication app)
+    {
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
     }
 }
