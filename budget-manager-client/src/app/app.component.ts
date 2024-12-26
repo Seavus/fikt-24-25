01e13a399e3ebf464fc6bbc5969ff989600e1f7c
Krenar Drala <krenar.drala@qinshift.com>
2024-12-26 15:48:39 +0100

    Sidebar changes

--- budget-manager-client/src/app/app.component.ts
+++ budget-manager-client/src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
-import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './shared/components/button/button.component';
-
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ SidebarComponent, CommonModule, ButtonComponent],
+  imports: [SidebarComponent, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'budget-manager-client';
