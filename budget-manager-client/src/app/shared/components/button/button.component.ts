32bc98fea3074085c4e65431bdfca803582ade70
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2024-12-28 23:12:18 +0100

    Button component added

--- budget-manager-client/src/app/shared/components/button/button.component.ts
+++ budget-manager-client/src/app/shared/components/button/button.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-button',
-  imports: [CommonModule],
+  imports: [MatButtonModule, CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
 
 @Input() text: string= '';
+@Input() color: string = '';
 @Input() buttonClass: string = '';
 @Output() onClick: EventEmitter<any> = new EventEmitter();
 
