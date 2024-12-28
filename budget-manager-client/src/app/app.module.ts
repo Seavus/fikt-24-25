da9b1f52ffcc1c162a8c2f4d1b5ce649a2d572bf
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2024-12-28 23:14:57 +0100

    Button component added

---
+++ budget-manager-client/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    MatButtonModule,
+  ],
+  providers: []
+})
+export class AppModule { }
