e815206f81bc163be9da3cd58ab61e4fb6a4eb9a
NikolaHristovskii <nicehristovski10@gmail.com>
2025-01-18 23:19:44 +0100

    ISSUE21:Implement-custom-exception-DomainException

---
+++ budget-manager-server/src/BudgetManager.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BudgetManager.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        
+        public DomainException(string message)
+            : base($"Domain exception: \"{message}\" throws from Domain Layer.")
+        {
+        }
+        
+    }
+}
