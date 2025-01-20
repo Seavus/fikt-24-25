dbc08c56e23828a398d1c347e4ac145f6d4e4f15
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-01-20 20:20:00 +0100

    Update IDomainEvent.cs
    Added a public modifier to EventId and setted to default new Guid value
    Added a public modifier to DateTime and setted to default UtcNow
    Added a public modifier to EventType and setted to typeof(IDomainEvent).AsseblyQualifiedName
    Used File Scoped Namespaces here

--- budget-manager-server/src/BudgetManager.Domain/Abstractions/IDomainEvent.cs
+++ budget-manager-server/src/BudgetManager.Domain/Abstractions/IDomainEvent.cs
@@ -5,12 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 using MediatR;
 
-namespace BudgetManager.Domain.Abstractions
-{
+namespace BudgetManager.Domain.Abstractions;
+
     public interface IDomainEvent : INotification
     {
-        Guid EventId { get; }
-        DateTime CreatedOn { get; }
-        string EventType { get; }
+        public Guid EventId { get; set; } = Guid.NewGuid();
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string EventType { get; set; } = typeof(IDomainEvent).AssemblyQualifiedName;
     }
-}
