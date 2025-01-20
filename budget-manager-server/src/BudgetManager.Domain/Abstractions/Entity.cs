bd53f51dfcd0def37c62cf3220a627688d149fe0
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-01-20 20:30:06 +0100

    Update Entity.cs
    Removed protected set
    Removed method SetAuditFields
    Removed method UpdateAuditFields
    Used File Scoped namespace here

--- budget-manager-server/src/BudgetManager.Domain/Abstractions/Entity.cs
+++ budget-manager-server/src/BudgetManager.Domain/Abstractions/Entity.cs
@@ -4,26 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BudgetManager.Domain.Abstractions
-{
+namespace BudgetManager.Domain.Abstractions;
     public abstract class Entity<T> : IEntity<T>
     {
-        public T Id { get; protected set; } = default!;
+        public T Id { get; set; } = default!;
         public DateTime CreatedOn { get; private set; }  = DateTime.UtcNow;
         public string CreatedBy { get; private set; } = "System";
         public DateTime? UpdatedOn { get; private set; }
         public string? UpdatedBy { get; private set;}
-
-        protected void SetAuditFields(string createdBy = "System")
-        {
-            CreatedOn = DateTime.UtcNow;
-            CreatedBy = createdBy;
-        }
-
-        protected void UpdateAuditFields(string updatedBy = "System")
-        {
-            UpdatedOn = DateTime.UtcNow;
-            UpdatedBy = updatedBy;
-        }
     }
-}
