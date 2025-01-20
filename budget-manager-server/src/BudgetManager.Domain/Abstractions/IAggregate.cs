0b223c10731d12b283af48ef87801b6cd89d2bb2
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-01-20 20:23:50 +0100

    Update IAggregate.cs
    Used File Scoped Namespace here

--- budget-manager-server/src/BudgetManager.Domain/Abstractions/IAggregate.cs
+++ budget-manager-server/src/BudgetManager.Domain/Abstractions/IAggregate.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BudgetManager.Domain.Abstractions
-{
+namespace BudgetManager.Domain.Abstractions;
+
     public interface IAggregate : IEntity
     {
         IReadOnlyCollection<IDomainEvent> DomainEvents { get; }
         void AddDomainEvent(IDomainEvent domainEvent);
         IDomainEvent[] ClearDomainEvents();
     }
-    public interface IAggregate<T> : IAggregate, IEntity<T>
-    {
-    }
-}
+    public interface IAggregate<T> : IAggregate, IEntity<T>;
