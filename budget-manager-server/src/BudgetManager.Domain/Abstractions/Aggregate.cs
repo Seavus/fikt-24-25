31842ea3b707ebe80c8be459b02ec89c88d98dd6
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-01-21 14:25:57 +0100

    Update Aggregate.cs
    Removed body of AddDomainEvent method
    Replaced DomainEvent with domainEvent

--- budget-manager-server/src/BudgetManager.Domain/Abstractions/Aggregate.cs
+++ budget-manager-server/src/BudgetManager.Domain/Abstractions/Aggregate.cs
@@ -10,12 +10,7 @@ namespace BudgetManager.Domain.Abstractions;
     {
         private readonly List<IDomainEvent> _domainEvents = new();
         public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
-        public void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(DomainEvent);
-        {
-            if (domainEvent == null)
-                throw new ArgumentNullException(nameof(domainEvent));
-            _domainEvents.Add(domainEvent);
-        }
+        public void AddDomainEvent(IDomainEvent domainEvent) => _domainEvents.Add(domainEvent);
         public IDomainEvent[] ClearDomainEvents()
         {
             var events = _domainEvents.ToArray();
