c9e46ef4be671d5395c8089bf134c2329a81dbca
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-01-21 17:20:03 +0100

    Update IDomainEvent.cs
    Removed not used namespaces here
    Fixed indentation

--- budget-manager-server/src/BudgetManager.Domain/Abstractions/IDomainEvent.cs
+++ budget-manager-server/src/BudgetManager.Domain/Abstractions/IDomainEvent.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MediatR;
 
 namespace BudgetManager.Domain.Abstractions;
 
-    public interface IDomainEvent : INotification
+public interface IDomainEvent : INotification
     {
         public Guid EventId => Guid.NewGuid();
         public DateTime CreatedOn => DateTime.UtcNow;
