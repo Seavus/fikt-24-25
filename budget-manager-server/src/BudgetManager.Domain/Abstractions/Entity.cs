fdcc7cc9f1bca6b710b75c8227d0f18405fab245
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-01-21 14:18:46 +0100

    Update Entity.cs
    Removed private modifier

--- budget-manager-server/src/BudgetManager.Domain/Abstractions/Entity.cs
+++ budget-manager-server/src/BudgetManager.Domain/Abstractions/Entity.cs
@@ -8,8 +8,8 @@ namespace BudgetManager.Domain.Abstractions;
     public abstract class Entity<T> : IEntity<T>
     {
         public T Id { get; set; } = default!;
-        public DateTime CreatedOn { get; private set; }  = DateTime.UtcNow;
-        public string CreatedBy { get; private set; } = "System";
-        public DateTime? UpdatedOn { get; private set; }
-        public string? UpdatedBy { get; private set;}
+        public DateTime CreatedOn { get; set; }  = DateTime.UtcNow;
+        public string CreatedBy { get; set; } = "System";
+        public DateTime? UpdatedOn { get; set; }
+        public string? UpdatedBy { get; set;}
     }
