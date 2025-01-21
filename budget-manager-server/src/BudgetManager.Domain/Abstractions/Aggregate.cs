bec4118bb7ffb66ac16f00c0ae16b82a011382cd
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-01-21 17:26:43 +0100

    Update Aggregate.cs
    Removed not used namespaces here
    Fixed indentation

--- budget-manager-server/src/BudgetManager.Domain/Abstractions/Aggregate.cs
+++ budget-manager-server/src/BudgetManager.Domain/Abstractions/Aggregate.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BudgetManager.Domain.Abstractions;
 
-    public abstract class Aggregate<T> : Entity<T>, IAggregate<T>
+public abstract class Aggregate<T> : Entity<T>, IAggregate<T>
     {
         private readonly List<IDomainEvent> _domainEvents = new();
         public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
