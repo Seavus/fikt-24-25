e9adf182f4b86857b8b1568eed1acdf4ab749a40
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2025-01-23 00:01:01 +0100

    added navigation routes to sidebar menu

--- budget-manager-client/src/app/core/sidebar/sidebar.component.ts
+++ budget-manager-client/src/app/core/sidebar/sidebar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, RouterModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
