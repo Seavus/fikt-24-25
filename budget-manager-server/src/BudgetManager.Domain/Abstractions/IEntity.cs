ea6530c00d143167c5a69a3b91a5a23ea720d9e7
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-01-23 16:16:24 +0100

    Update IEntity.cs

--- budget-manager-server/src/BudgetManager.Domain/Abstractions/IEntity.cs
+++ budget-manager-server/src/BudgetManager.Domain/Abstractions/IEntity.cs
@@ -1,16 +1,16 @@
-﻿using System;
+using System;
 
 namespace BudgetManager.Domain.Abstractions;
 
 public interface IEntity
-    {
-        DateTime CreatedOn { get; set; }
-        string CreatedBy { get; set; }
-        DateTime? UpdatedOn { get; set; }
-        string? UpdatedBy { get; set; }
-    }
+{
+    DateTime CreatedOn { get; set; }
+    string CreatedBy { get; set; }
+    DateTime? UpdatedOn { get; set; }
+    string? UpdatedBy { get; set; }
+}
 
 public interface IEntity<T> : IEntity
-    {
-        T Id { get; set; }
-    }
+{
+    T Id { get; set; }
+}
