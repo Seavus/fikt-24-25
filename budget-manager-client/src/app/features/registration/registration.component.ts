fa11b413b99decab10efa553556866e5a63912dd
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2025-01-23 14:28:01 +0100

    Replaced HTML forms with Angular Reactive Forms

--- budget-manager-client/src/app/features/registration/registration.component.ts
+++ budget-manager-client/src/app/features/registration/registration.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { InputComponent } from '../../shared/components/input/input.component';
 
+
 @Component({
   selector: 'app-registration',
-  imports: [ButtonComponent,InputComponent],
+  imports: [ButtonComponent,InputComponent, ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent {
+  registerForm;
+    constructor(private readonly fb: FormBuilder) {
+      this.registerForm = this.fb.group({
+      fullName: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required, Validators.minLength(8)]
+  });
+  }
+    
 
+  onSubmit(): void {
+    console.log('submitted form',
+       this.registerForm.value,
+       this.registerForm.invalid
+      );
+  }
 }
