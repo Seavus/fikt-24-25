93c503b5fb7aa049230a062cc1f399618a2a916e
Nikolaa00 <121193625+Nikolaa00@users.noreply.github.com>
2025-01-28 21:31:04 +0100

    Update User.cs

--- budget-manager-server/src/BudgetManager.Domain/Models/User.cs
+++ budget-manager-server/src/BudgetManager.Domain/Models/User.cs
@@ -2,25 +2,25 @@
 
 namespace BudgetManager.Domain.Models;
 
-public class User : Aggregate<Guid>
+public class User : Aggregate<UserId>
 {
     public string FirstName { get; private set; }
     public string LastName { get; private set; }
-    public string EmailName { get; private set; }
-    public string PasswordName { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
 
-    public User(Guid id, string firstName, string lasName, string email, string password)
+    private User(UserId id, string firstName, string lastName, string email, string password)
     {
         Id = id;
         FirstName = firstName;
-        LastName = lasName;
-        EmailName = email;
-        PasswordName = password;
+        LastName = lastName;
+        Email = email;
+        Password = password;
         CreatedOn = DateTime.UtcNow;
         CreatedBy = "System";
     }
 
-    public static User Create(Guid id, string firstName, string lastName, string email, string password)
+    public static User Create(UserId id, string firstName, string lastName, string email, string password)
     {
         if (string.IsNullOrWhiteSpace(firstName)) throw new ArgumentException("First name is required.");
         if (string.IsNullOrWhiteSpace(lastName)) throw new ArgumentException("Last name is required.");
