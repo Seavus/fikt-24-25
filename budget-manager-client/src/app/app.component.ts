0ec65c6453fb31a2d06ff91285281df567e00bfe
Teodora <bojadzieskat@gmail.com>
2025-02-13 17:32:16 +0100

    Remove imports in app.component.ts

--- budget-manager-client/src/app/app.component.ts
+++ budget-manager-client/src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './shared/components/button/button.component';
-import { TransactionsComponent } from './features/transactions/transactions.component';
-import { DynamicPopupComponent } from './shared/components/dynamic-popup/dynamic-popup.component';
 import { PopupData } from './shared/components/dynamic-popup/popup-data.model';
-
-
 
 
 @Component({
