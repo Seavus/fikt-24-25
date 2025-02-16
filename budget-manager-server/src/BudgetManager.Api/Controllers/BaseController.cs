a88bebbe8cc85e110e103c8cdd7f57133f7aafa4
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-02-16 19:29:24 +0100

    Created BaseController

---
+++ budget-manager-server/src/BudgetManager.Api/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetManager.Api.Controllers;
+
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    protected readonly IMapper Mapper;
+    protected readonly ISender Mediator;
+
+    protected BaseController(IMapper mapper, ISender mediator)
+    {
+        Mapper = mapper;
+        Mediator = mediator;
+    }
+}
