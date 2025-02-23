75aca573469af165f5a69804b89687b81a351431
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2025-02-24 00:31:05 +0100

    Add toggle functionality to sidebar

--- budget-manager-client/src/app/core/sidebar/sidebar.component.ts
+++ budget-manager-client/src/app/core/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { Router, RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -21,8 +21,30 @@ import { CommonModule } from '@angular/common';
 })
 export class SidebarComponent {
   constructor(private readonly router: Router) {}
+  @Input() isMenuOpened: boolean = false;
+  @Output() toggleMenu = new EventEmitter<void>();
 
   logout() {
     this.router.navigate(['/login']);
+  }
+
+  toggle() {
+    this.isMenuOpened = !this.isMenuOpened;
+
+    const pElements = document.querySelectorAll('.sidebar-nav li p');
+    const aElements = document.querySelectorAll('.sidebar-nav li a');
+
+    pElements.forEach((pElement: Element) => {
+      if (pElement instanceof HTMLElement) {
+        pElement.style.display = this.isMenuOpened ? 'inline' : 'none';
+      }
+    });
+    aElements.forEach((aElement: Element) => {
+      if (aElement instanceof HTMLElement) {
+        aElement.style.gap = '10px';
+      }
+    });
+
+    this.toggleMenu.emit();
   }
 }
