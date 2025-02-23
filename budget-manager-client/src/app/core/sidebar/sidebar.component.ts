97906bd9f95ad03ffea845a8421f9f2593379060
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2025-02-23 20:33:57 +0100

    Implement responsive design for sidebar navigation

--- budget-manager-client/src/app/core/sidebar/sidebar.component.ts
+++ budget-manager-client/src/app/core/sidebar/sidebar.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { Router, RouterModule } from '@angular/router';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [ButtonComponent, RouterModule, MatSidenavModule],
+  imports: [
+    ButtonComponent,
+    RouterModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    CommonModule,
+  ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
   constructor(private readonly router: Router) {}
