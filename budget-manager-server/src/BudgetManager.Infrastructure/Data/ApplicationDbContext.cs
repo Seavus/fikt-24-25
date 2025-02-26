cabc5810524abee711416b13f3fbab8f875c003d
NikolaHristovskii <nicehristovski10@gmail.com>
2025-02-26 22:17:51 +0100

    ISSUE71:Create-entity-type-configurations-for-User-and-Category-entity

--- budget-manager-server/src/BudgetManager.Infrastructure/Data/ApplicationDbContext.cs
+++ budget-manager-server/src/BudgetManager.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using BudgetManager.Domain.Models;
 using BudgetManager.Application.Data;
 using Microsoft.EntityFrameworkCore;
+using BudgetManager.Domain.Models.ValueObjects;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BudgetManager.Infrastructure.Data;
 
@@ -10,11 +12,29 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
     {
     }
 
-    public DbSet<User> Users => Set<User>(); 
+    public DbSet<User> Users => Set<User>();
     public DbSet<Category> Categories => Set<Category>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        var categoryIdConverter = new ValueConverter<CategoryId, Guid>(
+        id => id.Value,
+        value => new CategoryId(value)
+    );
+
+        var userIdConverter = new ValueConverter<UserId, Guid>(
+            id => id.Value,
+            value => new UserId(value)
+        );
+
+        modelBuilder.Entity<Category>()
+            .Property(c => c.Id)
+            .HasConversion(categoryIdConverter);
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.Id)
+            .HasConversion(userIdConverter);
+
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
     }
---
+++ budget-manager-server/src/BudgetManager.Infrastructure/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using BudgetManager.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BudgetManager.Infrastructure.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(c => c.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(c => c.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(c => c.Password)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
