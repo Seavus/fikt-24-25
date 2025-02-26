a8318c9dc67b66685ebf263d89385bfd780d825c
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-02-26 14:53:59 +0100

    Implemented Post Login Api Method In AccountController (#84)
    * Implemented Post Login Api Method In AccountController
    
    * Added route  [HttpPost("token")]
    Corrected string keyword mistakes
    Deleted values controller
    
    * Changed comment
    
    * revert changes
    
    ---------
    
    Co-authored-by: Zoran Gjorgjievski <zoran.gjorgjievski@qinshift.com>

--- budget-manager-server/src/BudgetManager.Api/Controllers/AccountController.cs
+++ budget-manager-server/src/BudgetManager.Api/Controllers/AccountController.cs
@@ -1,8 +1,9 @@
 ﻿using BudgetManager.Application.Users.RegisterUser;
+using BudgetManager.Application.Users.LoginUser;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace BudgetManager.Api.Controllers;
-
 
 [ApiController]
 [Route("api/account")]
@@ -36,6 +37,39 @@ public class AccountController : BaseController
         catch (Exception ex)
         {
             return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred:  {ex.Message}");
+        }
+    }
+
+    /// <summary>
+    /// Returns token for authenticated users.
+    /// </summary>
+    [HttpPost("token")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(LoginUserResponse))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public IActionResult Login([FromBody] LoginUserRequest request)
+    {
+        if (request == null)
+        {
+            throw new ArgumentNullException(nameof(request));
+        }
+        if (string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.Password))
+        {
+            return BadRequest("Invalid login request.");
+        }
+        try
+        {
+            if (request.Email == "admin" && request.Password == "admin")
+            {
+                var result = new LoginUserResponse("mocked-jwt-token");
+                return Ok(result);
+            }
+            return Unauthorized("Invalid credentials.");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, $"An error occurred: {ex.Message}");
         }
     }
 }
