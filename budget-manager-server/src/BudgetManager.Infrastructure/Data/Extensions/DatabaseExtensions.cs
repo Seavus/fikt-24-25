b8fef534a183920d208716dd39289b0f78d88613
NikolaHristovskii <nicehristovski10@gmail.com>
2025-03-03 10:14:49 +0100

    Implemented issue: Seed the database with users and categories with a runtime error

---
+++ budget-manager-server/src/BudgetManager.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudgetManager.Infrastructure.Data.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task SeedAsync(ApplicationDbContext context)
+    {
+        await SeedUsersAsync(context);
+        await context.SaveChangesAsync();
+
+        await SeedCategoriesAsync(context);
+    }
+
+    private static async Task SeedUsersAsync(ApplicationDbContext context)
+    {
+        if (!context.Users.Any())
+        {
+            await context.Users.AddRangeAsync(InitialData.Users);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static async Task SeedCategoriesAsync(ApplicationDbContext context)
+    {
+        if (!context.Categories.Any())
+        {
+            await context.Categories.AddRangeAsync(InitialData.Categories);
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        await context.Database.MigrateAsync();
+        await DatabaseExtensions.SeedAsync(context);
+    }
+}
---
+++ budget-manager-server/src/BudgetManager.Infrastructure/Data/Extensions/InitialData.cs
@@ -0,0 +1,19 @@
+﻿using BudgetManager.Domain.Models.ValueObjects;
+using BudgetManager.Domain.Models;
+
+namespace BudgetManager.Infrastructure.Data.Extensions;
+
+internal static class InitialData
+{
+    public static List<User> Users => new()
+    {
+        User.Create(UserId.Create(Guid.NewGuid()), "John", "Doe", "john@example.com", "User_0123"),
+        User.Create(UserId.Create(Guid.NewGuid()), "Jane", "Smith", "jane@example.com", "User_1234")
+    };
+
+    public static List<Category> Categories => new()
+    {
+        Category.Create ( CategoryId.Create(Guid.NewGuid()), "Food", Users[0].Id ),
+        Category.Create ( CategoryId.Create(Guid.NewGuid()), "Transport", Users[1].Id )
+    };
+}
