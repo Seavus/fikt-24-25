53bcead018b25c43ebe92e23550b5caafa6cda64
NikolaHristovskii <nicehristovski10@gmail.com>
2025-03-04 22:46:19 +0100

    Implement-register-user-command-73

---
+++ budget-manager-server/src/BudgetManager.Application/Users/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace BudgetManager.Application.Users.RegisterUser
+{
+    public record RegisterUserCommand(string Firstname, string Lastname, string Email, string Password) : IRequest<RegisterUserResponse>;
+}
---
+++ budget-manager-server/src/BudgetManager.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using BudgetManager.Domain.Models;
+using MediatR;
+using BudgetManager.Application.Data;
+using BudgetManager.Domain.Models.ValueObjects;
+
+namespace BudgetManager.Application.Users.RegisterUser
+{
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, RegisterUserResponse>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public RegisterUserCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<RegisterUserResponse> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = User.Create(
+                new UserId(Guid.NewGuid()),  
+                request.Firstname,
+                request.Lastname,
+                request.Email,
+                request.Password
+            );
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new RegisterUserResponse(user.Id.Value); 
+        }
+    }
+}
