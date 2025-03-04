de7531873bc9bcf0d04022ca3fec6091caeff228
NikolaHristovskii <nicehristovski10@gmail.com>
2025-03-04 09:33:47 +0100

    Small changes

--- budget-manager-server/src/BudgetManager.Infrastructure/Data/Extensions/InitialData.cs
+++ budget-manager-server/src/BudgetManager.Infrastructure/Data/Extensions/InitialData.cs
@@ -5,8 +5,6 @@ namespace BudgetManager.Infrastructure.Data.Extensions;
 
 internal static class InitialData
 {
-    private static readonly Guid User1Id = new Guid("55b13cc2-b24b-4501-9c53-af6ade254194");
-    private static readonly Guid User2Id = new Guid("ee8b09a5-6c99-4013-bca6-60b4c6f5c1f0");
     public static List<User> Users => new()
     {
         User.Create(UserId.Create(new Guid("55b13cc2-b24b-4501-9c53-af6ade254194")), "John", "Doe", "john@example.com", "User_0123"),
@@ -15,7 +13,7 @@ internal static class InitialData
 
 public static List<Category> Categories => new()
     {
-        Category.Create ( CategoryId.Create(new Guid("46f191dd-acec-4144-85ce-30f2237b0403")), "Food", UserId.Create(User1Id) ),
-        Category.Create ( CategoryId.Create(new Guid("9d997320-1eb1-4bbe-b609-2b6e4861eeb7")), "Transport", UserId.Create(User2Id) )
+        Category.Create ( CategoryId.Create(new Guid("46f191dd-acec-4144-85ce-30f2237b0403")), "Food", UserId.Create(new Guid("55b13cc2-b24b-4501-9c53-af6ade254194")) ),
+        Category.Create ( CategoryId.Create(new Guid("9d997320-1eb1-4bbe-b609-2b6e4861eeb7")), "Transport", UserId.Create(new Guid("ee8b09a5-6c99-4013-bca6-60b4c6f5c1f0")))
     };
 }
