948c8feb5957a1f5598ef396bdff2f5757c333da
NikolaHristovskii <nicehristovski10@gmail.com>
2025-03-07 19:16:57 +0100

    ISSUE78:Create-current-user-service-for-storing-and-reading-current-logged-in-user

---
+++ budget-manager-server/src/BudgetManager.Infrastructure/Data/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using BudgetManager.Infrastructure.Middlewares;
+
+namespace BudgetManager.Infrastructure.Extensions;
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseCurrentUser(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<CurrentUserMiddleware>();
+    }
+}
---
+++ budget-manager-server/src/BudgetManager.Infrastructure/Data/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using BudgetManager.Application.Services;
+using BudgetManager.Infrastructure.Middlewares;
+using BudgetManager.Infrastructure.Services;
+
+namespace BudgetManager.Infrastructure.Extensions;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCurrentUser(this IServiceCollection services)
+    {
+        services.AddScoped<ICurrentUser, CurrentUser>();
+        services.AddScoped<CurrentUserMiddleware>();
+        return services;
+    }
+}
