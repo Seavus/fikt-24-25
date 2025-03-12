9f55439fe22da338a6ddf651907d28ccc3ba4b67
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-03-12 16:51:21 +0100

    Created Validation Behavior Pipeline for validating our requests with FluentValidation Package

--- budget-manager-server/src/BudgetManager.Application/DependencyInjection.cs
+++ budget-manager-server/src/BudgetManager.Application/DependencyInjection.cs
@@ -1,10 +1,16 @@
-﻿namespace BudgetManager.Application;
+﻿using BudgetManager.Application.Behaviors;
+
+namespace BudgetManager.Application;
 
 public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+        services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         return services;
     }
 }
