441b3db315fdc04a0ee5eb2f773d19d71cae2f0f
ChavleskaEmanuela <116969263+ChavleskaEmanuela@users.noreply.github.com>
2025-03-13 16:48:29 +0100

    Remove unused import of RouterOutlet

--- budget-manager-client/src/app/app.component.ts
+++ budget-manager-client/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { Component } from '@angular/core';
 import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { PopupData } from './shared/components/dynamic-popup/popup-data.model';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, CommonModule, MatSidenavModule,RouterOutlet],
+  imports: [SidebarComponent, CommonModule, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
