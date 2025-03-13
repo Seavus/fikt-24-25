dd9e6ec33c8b03edeb1267e497734058faa8bba7
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-03-13 16:36:08 +0100

    Fixed indentation

--- budget-manager-server/src/BudgetManager.Application/DependencyInjection.cs
+++ budget-manager-server/src/BudgetManager.Application/DependencyInjection.cs
@@ -12,6 +12,7 @@ public static class DependencyInjection
 
             cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
-            return services;
+        
+        return services;
     }
 }
