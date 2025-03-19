25ddd946dd486911ffec7009112c5dbd8fce25a0
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-03-19 20:36:58 +0100

    *uncommited change

--- budget-manager-server/src/BudgetManager.Infrastructure/Data/ApplicationDbContext.cs
+++ budget-manager-server/src/BudgetManager.Infrastructure/Data/ApplicationDbContext.cs
@@ -6,16 +6,27 @@ namespace BudgetManager.Infrastructure.Data;
 
 public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    private readonly IConfiguration _configuration;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
     {
+        _configuration = configuration;
     }
 
     public DbSet<User> Users => Set<User>();
     public DbSet<Category> Categories => Set<Category>();
-
+    public DbSet<Transaction> Transactions => Set<Transaction>();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
         base.OnModelCreating(modelBuilder);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString, options => options.MigrationsAssembly("BudgetManager.Infrastructure"));
+        }
     }
 }
