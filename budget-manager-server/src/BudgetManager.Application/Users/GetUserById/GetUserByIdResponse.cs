0cb807b3db028de89f57ef43660a75d701bdcb95
Nikolaa00 <121193625+Nikolaa00@users.noreply.github.com>
2025-03-27 10:56:16 +0100

    ISSUE96:Implement DispatchDomainEventsInterceptor interceptor for publishing domain events (#130)
    * ISSUE96:Implement DispatchDomainEventsInterceptor interceptor for publishing domain events
    
    * Update DispatchDomainEventsInterceptor.cs
    
    * minor changes
    
    ---------
    
    Co-authored-by: NikolaHristovskii <nicehristovski10@gmail.com>

--- budget-manager-server/src/BudgetManager.Application/Users/GetUserById/GetUserByIdResponse.cs
+++ budget-manager-server/src/BudgetManager.Application/Users/GetUserById/GetUserByIdResponse.cs
@@ -3,11 +3,11 @@
 public record GetUserByIdResponse
 {
     public Guid Id { get; init; }
-    public string FirstName { get; init; }
-    public string LastName { get; init; }
-    public string Email { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
     public DateTime CreatedOn { get; init; }
-    public string CreatedBy { get; init; }
+    public required string CreatedBy { get; init; }
     public DateTime? UpdatedOn { get; init; }
     public string? UpdatedBy { get; init; }
 
