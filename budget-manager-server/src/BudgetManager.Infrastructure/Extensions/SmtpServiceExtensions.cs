b2f98fcddc97cfc5370696ba70f31558657f4f08
NikolaHristovskii <nicehristovski10@gmail.com>
2025-04-13 20:52:06 +0200

    replaced braces with semicolon

--- budget-manager-server/src/BudgetManager.Infrastructure/Extensions/SmtpServiceExtensions.cs
+++ budget-manager-server/src/BudgetManager.Infrastructure/Extensions/SmtpServiceExtensions.cs
@@ -1,23 +1,21 @@
-﻿namespace BudgetManager.Infrastructure.Extensions
+﻿namespace BudgetManager.Infrastructure.Extensions;
+public static class SmtpServiceExtensions
 {
-    public static class SmtpServiceExtensions
+    private static FluentEmailServicesBuilder AddFluentEmailDefault(this IServiceCollection services)
     {
-        private static FluentEmailServicesBuilder AddFluentEmailDefault(this IServiceCollection services)
-        {
-            return services.AddFluentEmail("no-reply@budgetmanager.com");
-        }
+        return services.AddFluentEmail("no-reply@budgetmanager.com");
+    }
 
-        private static IServiceCollection AddSmtpSender(this IServiceCollection services)
-        {
-            services.AddFluentEmailDefault()
-                    .AddSmtpSender("localhost", 25);
+    private static IServiceCollection AddSmtpSender(this IServiceCollection services)
+    {
+        services.AddFluentEmailDefault()
+                .AddSmtpSender("localhost", 25);
 
-            return services;
-        }
+        return services;
+    }
 
-        public static IServiceCollection AddSmtpMail(this IServiceCollection services)
-        {
-            return services.AddSmtpSender();
-        }
+    public static IServiceCollection AddSmtpMail(this IServiceCollection services)
+    {
+        return services.AddSmtpSender();
     }
 }
