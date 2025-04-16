0c3d3c8b4eeba28c4fb36f1293052f9521e24966
NikolaHristovskii <nicehristovski10@gmail.com>
2025-04-16 11:44:51 +0200

    Moved SMTP extension in DependencyInjection

--- budget-manager-server/src/BudgetManager.Infrastructure/DependencyInjection.cs
+++ budget-manager-server/src/BudgetManager.Infrastructure/DependencyInjection.cs
@@ -207,4 +207,23 @@ public static class DependencyInjection
         services.AddScoped<CurrentUserMiddleware>();
         return services;
     }
+
+    private static FluentEmailServicesBuilder AddFluentEmailDefault(this IServiceCollection services)
+    {
+        return services.AddFluentEmail("no-reply@budgetmanager.com");
+    }
+
+    private static IServiceCollection AddSmtpSender(this IServiceCollection services)
+    {
+        services.AddFluentEmailDefault()
+                .AddSmtpSender("localhost", 25);
+
+        return services;
+    }
+
+    public static IServiceCollection AddSmtpMail(this IServiceCollection services)
+    {
+        return services.AddSmtpSender();
+    }
+
 }
