9d434c4a15877fb83db01195144ce3eac5b08c38
bojan-oth <trajkovski.bojan@uklo.edu.mk>
2025-04-17 19:05:18 +0200

    Implemented Get Categories By User

---
+++ budget-manager-server/src/BudgetManager.Application/Users/GetCatogiresByUser/GetCategoriesByUserQuery.cs
@@ -0,0 +1,5 @@
+﻿using BudgetManager.Application.Common.Responses;
+
+namespace BudgetManager.Application.Users.GetCategoriesByUser;
+
+public record GetCategoriesByUserQuery(Guid UserId, int PageIndex, int PageSize) : IRequest<PaginatedResponse<GetCategoriesByUserResponse>>;
---
+++ budget-manager-server/src/BudgetManager.Application/Users/GetCatogiresByUser/GetCategoriesByUserQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using BudgetManager.Application.Common.Responses;
+using BudgetManager.Application.Data;
+
+namespace BudgetManager.Application.Users.GetCategoriesByUser;
+
+internal class GetCategoriesByUserQueryHandler : IRequestHandler<GetCategoriesByUserQuery, PaginatedResponse<GetCategoriesByUserResponse>>
+{
+    private IApplicationDbContext _context;
+
+    public GetCategoriesByUserQueryHandler(IApplicationDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<PaginatedResponse<GetCategoriesByUserResponse>> Handle(GetCategoriesByUserQuery request, CancellationToken cancellationToken)
+    {
+        var query = _context.Categories.AsQueryable();
+
+        int totalCount = await query.CountAsync(cancellationToken);
+
+        var categories = await query
+            .OrderBy(c => c.Name)
+            .Skip((request.PageIndex - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .Select(c => new GetCategoriesByUserResponse(c.Id.Value, c.Name))
+            .ToListAsync(cancellationToken);
+
+        return new PaginatedResponse<GetCategoriesByUserResponse>(categories, request.PageIndex, request.PageSize, totalCount);
+
+    }
+}
