09b28c6e5546786a93830ac6c471c8edf7218469
NikolaHristovskii <nicehristovski10@gmail.com>
2025-05-14 01:29:20 +0200

    Implemented Delete transaction endpoint

--- budget-manager-server/src/BudgetManager.Api/Controllers/TransactionsController.cs
+++ budget-manager-server/src/BudgetManager.Api/Controllers/TransactionsController.cs
@@ -1,4 +1,6 @@
-﻿using BudgetManager.Application.Transactions.CreateTransaction;
+﻿using BudgetManager.Application.Categories.DeleteCategory;
+using BudgetManager.Application.Transactions.CreateTransaction;
+using BudgetManager.Application.Transactions.DeleteTransaction;
 using Microsoft.AspNetCore.Authorization;
 
 namespace BudgetManager.Api.Controllers;
@@ -26,5 +28,20 @@ public class TransactionsController : BaseController
 
         var response = await Mediator.Send(command);
         return Created($"api/transactions/{response.TransactionId}", response);
+    }
+
+    /// <summary>
+    /// Delete transaction.
+    /// </summary>
+    [HttpDelete("{transactionId:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(DeleteTransactionResponse))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> DeleteTransaction([FromRoute] Guid transactionId)
+    {
+        var command = new DeleteTransactionCommand(transactionId);
+        var result = await Mediator.Send(command);
+        return Ok(result);
     }
 }
---
+++ budget-manager-server/src/BudgetManager.Application/Transactions/DeleteTransaction/DeleteTransactionHandler.cs
@@ -0,0 +1,49 @@
+﻿using BudgetManager.Application.Data;
+using BudgetManager.Application.Exceptions;
+using BudgetManager.Application.Services;
+using BudgetManager.Domain.Enums;
+using BudgetManager.Domain.Models.ValueObjects;
+
+namespace BudgetManager.Application.Transactions.DeleteTransaction;
+internal sealed class DeleteTransactionHandler : IRequestHandler<DeleteTransactionCommand, DeleteTransactionResponse>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly ICurrentUser _currentUser;
+
+    public DeleteTransactionHandler(IApplicationDbContext context, ICurrentUser currentUser)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _currentUser = currentUser ?? throw new ArgumentNullException(nameof(currentUser));
+    }
+
+    public async Task<DeleteTransactionResponse> Handle(DeleteTransactionCommand request, CancellationToken cancellationToken)
+    {
+        var transactionId = TransactionId.Create(request.TransactionId);
+
+        var transaction = await _context.Transactions
+            .FirstOrDefaultAsync(t => t.Id == transactionId, cancellationToken);
+
+        if (transaction == null)
+            throw new NotFoundException("Transaction not found");
+
+        var userId = UserId.Create(_currentUser.UserId!.Value);
+
+        var user = await _context.Users
+            .FirstOrDefaultAsync(u => u.Id == userId, cancellationToken);
+
+        switch (transaction.TransactionType)
+        {
+            case TransactionType.Income:
+                user.CreditBalance(transaction.Amount);
+                break;
+            case TransactionType.Expense:
+                user.DebitBalance(transaction.Amount);
+                break;
+        }
+
+        transaction.SetDeleted();
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return new DeleteTransactionResponse(true);
+    }
+}
